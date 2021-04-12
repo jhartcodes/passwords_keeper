5feafe1d2c6db448e8284d5c544499464a01f883
Scarlet Shao <scarletshao@gmail.com>
2021-04-12 22:12:38 +0000

    final version

--- db/seeds/03_passwords.sql
+++ db/seeds/03_passwords.sql
@@ -1,4 +1,15 @@
 INSERT INTO passwords
-  (password, encrypted_password, url, type, current_user_id , employee_id)
+  (password, url, type, username)
 VALUES
-  ('12345','54321', 'www.facebook.com' , 'social' , 1 , NULL );
+  ('12345','www.facebook.com' , 'social' , 'happy' ),
+  ('786423','www.amazon.ca' , 'work-related' , 'today' ),
+  ('45677','www.ebay.com' , 'entertainment' , 'now' ),
+  ('12345','www.gmail.com' , 'social' , 'happytmr' ),
+  ('786423','www.lighthouse.com' , 'work-related' , 'Andy' ),
+  ('45677','www.github.com' , 'entertainment' , 'Gary' ),
+  ('12345','www.hi.com' , 'social' , 'Scarlet' ),
+  ('786423','www.amazon.ca' , 'work-related' , 'Joel' ),
+  ('45677','www.ebay.com' , 'entertainment' , 'Nally' );
+
+
+
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,7 +12,7 @@ $(() => {
       const $username = $('<td>').addClass('encrypted_password').text(user.username)
       const $type = $('<td>').addClass('encrypted_password').text(user.type)
       const $url = $('<a>').attr("href", `https://${user.url}`).addClass('url').text(user.url)
-      $passwordtable.append($passwords)
+      $passwordtable.prepend($passwords)
       $passwords.append($username, $password, $url, $type)
     }
   });
